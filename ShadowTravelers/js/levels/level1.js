26eccd556fd1b52c7d999da76ab458384facc31e
moncada jeremy <jeremy.moncada@etu.unice.fr>
2025-02-24 18:40:11 +0100

    correction personnage shadowtravelers

--- ShadowTravelers/js/levels/level1.js
+++ ShadowTravelers/js/levels/level1.js
@@ -3,31 +3,48 @@ export class Level1 {
         this.context = context;
         this.canvas = canvas;
         
+        this.floorHeight = 50; // hauteur du sol noir
+        
         this.player = {
             x: this.canvas.width / 2,
-            y: this.canvas.height + 455,
+            y: 0, // sera calculé dans le update
             width: 100,
             height: 100,
             speed: 5,
             direction: 'right',
-            lastDirection: 'right'
+            lastDirection: 'right',
+            isJumping: false,
+            jumpForce: -15,
+            gravity: 0.3,
+            velocityY: 0,
+            groundY: 0 // sera calculé dans le update
         };
 
         this.playerSprites = {
             runRight: new Image(),
             runLeft: new Image(),
             idleRight: new Image(),
-            idleLeft: new Image()
+            idleLeft: new Image(),
+            jumpRight: new Image(),
+            jumpLeft: new Image(),
+            fallRight: new Image(),
+            fallLeft: new Image()
         };
 
         this.playerSprites.runRight.src = './assets/player/run-right.png';
         this.playerSprites.runLeft.src = './assets/player/run-left.png';
         this.playerSprites.idleRight.src = './assets/player/idle-right.png';
         this.playerSprites.idleLeft.src = './assets/player/idle-left.png';
+        this.playerSprites.jumpRight.src = './assets/player/fall-right.png';
+        this.playerSprites.jumpLeft.src = './assets/player/fall-left.png';
+        this.playerSprites.fallRight.src = './assets/player/fall-right.png';
+        this.playerSprites.fallLeft.src = './assets/player/fall-left.png';
 
         this.keys = {
             ArrowLeft: false,
-            ArrowRight: false
+            ArrowRight: false,
+            Space: false,
+            ArrowUp: false
         };
 
         this.bindEvents();
@@ -47,7 +64,13 @@ export class Level1 {
         });
     }
 
-    update() {        
+    update() {
+        this.player.groundY = this.canvas.height - this.floorHeight - this.player.height;
+        
+        if (!this.player.isJumping) {
+            this.player.y = this.player.groundY;
+        }
+        
         if (this.keys.ArrowLeft) {
             this.player.x -= this.player.speed;
             this.player.direction = 'left';
@@ -59,6 +82,22 @@ export class Level1 {
             this.player.lastDirection = 'right';
         }
 
+        if ((this.keys.Space || this.keys.ArrowUp) && !this.player.isJumping) {
+            this.player.isJumping = true;
+            this.player.velocityY = this.player.jumpForce;
+        }
+
+        if (this.player.isJumping) {
+            this.player.velocityY += this.player.gravity;
+            this.player.y += this.player.velocityY;
+
+            if (this.player.y >= this.player.groundY) {
+                this.player.y = this.player.groundY;
+                this.player.isJumping = false;
+                this.player.velocityY = 0;
+            }
+        }
+
         this.player.x = Math.max(0, Math.min(this.canvas.width - this.player.width, this.player.x));
     }
 
@@ -66,10 +105,18 @@ export class Level1 {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
         this.context.fillStyle = 'black';
-        this.context.fillRect(0, this.canvas.height - 50, this.canvas.width, 50);
+        this.context.fillRect(0, this.canvas.height - this.floorHeight, this.canvas.width, this.floorHeight);
 
         let sprite;
-        if (this.keys.ArrowLeft || this.keys.ArrowRight) {
+        if (this.player.isJumping) {
+            if (this.player.velocityY > 0) {
+                // Si la vélocité est positive, le joueur est en train de tomber
+                sprite = this.player.lastDirection === 'right' ? this.playerSprites.fallRight : this.playerSprites.fallLeft;
+            } else {
+                // Si la vélocité est négative, le joueur est en train de monter
+                sprite = this.player.lastDirection === 'right' ? this.playerSprites.jumpRight : this.playerSprites.jumpLeft;
+            }
+        } else if (this.keys.ArrowLeft || this.keys.ArrowRight) {
             sprite = this.player.direction === 'right' ? this.playerSprites.runRight : this.playerSprites.runLeft;
         } else {
             sprite = this.player.lastDirection === 'right' ? this.playerSprites.idleRight : this.playerSprites.idleLeft;
